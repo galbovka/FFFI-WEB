09998d47121660cc09835bbd474623467174519e
Jan Sláma <qortimor@email.cz>
2015-04-20 09:24:21 +0200

    Reorganizace do složek
    Snad to bude díky tomu přehlednější. To, co je společné, jsem nechal v
    hlavní složce

--- index.php
+++
@@ -1,5 +0,0 @@
-#!/packages/run/php/bin/php
-<?php
-header("Location: http://festival.fi.muni.cz/2015/index.cs.html");
-exit();
-?>
---
+++ php/index.php
@@ -0,0 +1,5 @@
+#!/packages/run/php/bin/php
+<?php
+header("Location: http://festival.fi.muni.cz/test/index.cs.php");
+exit();
+?>
