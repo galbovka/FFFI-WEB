1bff235ebc456b80bdecd5ea8bab865442fecec7
Jan Sláma <qortimor@email.cz>
2015-04-22 15:08:25 +0200

    Změna EOL
    z DOS na UNIX

--- php/test.php
+++ php/test.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="pragma" content="no-cache" />
-	<meta http-equiv="cache-control" content="no-cache" />
-	<meta http-equiv="Content-Language" content="cs" />
-	<meta name="author" content="Anna Galbova, FI MU; Jan Slama, FI MU" />
-	<meta name="copyright" content="LEMMA" />
-	<meta name="robots" content="all,follow" />
-	<meta name="keywords" content="LEMMA PV113 Fakulta informatiky MU multimedia publishing filmová tvorba Softwarové elektronické publikace - seminá filmový festival" />
-	<meta name="description" content="FF - LEMMA | Stránky Filmového festivalu Masarykovy univerzity Fakulty informatiky v&nbsp;Brně." />
-
-  <title>Test</title>
-
-  <!--[if IE]>
-  <link rel="stylesheet" type="text/css" href="ie.css" />
-  <![endif]-->
-  
-</head>
-
-<body>
+#!/packages/run/php/bin/php
 <?php phpversion();?>
-</body>
-</html>
+
