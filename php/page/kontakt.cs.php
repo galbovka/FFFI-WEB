9539ca18e47960f22f3cd926b4fdc55e1dd2aaf3
Jan Sláma <qortimor@email.cz>
2015-05-05 10:27:54 +0200

    Úprava odkazů v kontaktech
    Linky mimo stránku upraveny na target="_blank"

--- php/page/kontakt.cs.php
+++ php/page/kontakt.cs.php
@@ -20,11 +20,11 @@
     <div class="mapa" style="text-align: center;"><br /><iframe width="90%" height="350" frameborder="0" marginwidth="auto" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=masarykova+univerzita+fakultainformatiky&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=41.360684,107.138672&amp;ie=UTF8&amp;hq=masarykova+univerzita+fakultainformatiky&amp;hnear=&amp;radius=15000&amp;t=m&amp;ll=49.209776,16.599097&amp;spn=0.009813,0.032229&amp;z=15&amp;iwloc=near&amp;output=embed"></iframe></div>
     <h2>Doprava</h2>
     <p>Nejsnadněji se dá na fakultu dostat z&nbsp;trolejbusové zastávky Botanická linkou 
-				<a href="http://jizdnirady.idnes.cz/brno/zjr/?date=15.5.2015+%c4%8ct&l=32+-+Trol&f=Botanick%c3%a1&t=KR%c3%81LOVO+POLE%2c+Srbsk%c3%a1&wholeweek=false&submit=true&af=false&lng=C">
+				<a href="http://jizdnirady.idnes.cz/brno/zjr/?date=15.5.2015+%c4%8ct&l=32+-+Trol&f=Botanick%c3%a1&t=KR%c3%81LOVO+POLE%2c+Srbsk%c3%a1&wholeweek=false&submit=true&af=false&lng=C" target="_blank">
 				32</a> nebo z&nbsp;tramvajové zastávky Hrnčírská linkami 
-				<a href="http://jizdnirady.idnes.cz/brno/zjr/?date=15.5.2015+%c4%8ct&l=1+-+Tram&f=Hrn%c4%8d%c3%ad%c5%99sk%c3%a1&t=%c5%98e%c4%8dkovice&wholeweek=false&submit=true&af=false&lng=C">
+				<a href="http://jizdnirady.idnes.cz/brno/zjr/?date=15.5.2015+%c4%8ct&l=1+-+Tram&f=Hrn%c4%8d%c3%ad%c5%99sk%c3%a1&t=%c5%98e%c4%8dkovice&wholeweek=false&submit=true&af=false&lng=C" target="_blank">
 				1</a> a 
-				<a href="http://jizdnirady.idnes.cz/brno/zjr/?date=15.5.2015+%c4%8ct&l=6+-+Tram&f=Hrn%c4%8d%c3%ad%c5%99sk%c3%a1&t=Kr%c3%a1lovo+Pole%2c+n%c3%a1dra%c5%be%c3%ad&wholeweek=false&submit=true&af=false&lng=C">
+				<a href="http://jizdnirady.idnes.cz/brno/zjr/?date=15.5.2015+%c4%8ct&l=6+-+Tram&f=Hrn%c4%8d%c3%ad%c5%99sk%c3%a1&t=Kr%c3%a1lovo+Pole%2c+n%c3%a1dra%c5%be%c3%ad&wholeweek=false&submit=true&af=false&lng=C" target="_blank">
 				6</a>.
 		</p>
     <p class="podpis"></p>
